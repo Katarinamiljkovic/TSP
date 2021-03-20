f78b86fd179985e505363478ea9d2e37f4eedeb8
Katarinamiljkovic <62013804+Katarinamiljkovic@users.noreply.github.com>
2021-03-20 22:49:50 +0100

    probica

---
+++ app/src/main/java/kod/tsp/MainActivity.java
@@ -0,0 +1,123 @@
+package kod.tsp;
+
+import android.content.Intent;
+import android.os.Bundle;
+
+import com.google.android.gms.tasks.OnCompleteListener;
+import com.google.android.gms.tasks.Task;
+import com.google.android.material.floatingactionbutton.FloatingActionButton;
+import com.google.android.material.snackbar.Snackbar;
+
+import androidx.annotation.NonNull;
+import androidx.appcompat.app.AppCompatActivity;
+import androidx.appcompat.widget.Toolbar;
+
+import com.google.firebase.firestore.DocumentReference;
+import com.google.firebase.firestore.DocumentSnapshot;
+import com.google.firebase.firestore.FirebaseFirestore;
+import com.google.firebase.firestore.QueryDocumentSnapshot;
+import com.google.firebase.firestore.QuerySnapshot;
+
+import android.util.Log;
+import android.view.View;
+
+import android.view.Menu;
+import android.view.MenuItem;
+import android.widget.Button;
+import android.widget.EditText;
+
+import java.util.ArrayList;
+
+public class MainActivity extends AppCompatActivity {
+
+    FirebaseFirestore db = FirebaseFirestore.getInstance();
+
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+        Toolbar toolbar = findViewById(R.id.toolbar);
+        setSupportActionBar(toolbar);
+
+
+    }
+
+    @Override
+    public boolean onCreateOptionsMenu(Menu menu) {
+        // Inflate the menu; this adds items to the action bar if it is present.
+        getMenuInflater().inflate(R.menu.menu_main, menu);
+        return true;
+    }
+
+    @Override
+    public boolean onOptionsItemSelected(MenuItem item) {
+        // Handle action bar item clicks here. The action bar will
+        // automatically handle clicks on the Home/Up button, so long
+        // as you specify a parent activity in AndroidManifest.xml.
+        int id = item.getItemId();
+
+        //noinspection SimplifiableIfStatement
+        if (id == R.id.action_settings) {
+            return true;
+        }
+
+        return super.onOptionsItemSelected(item);
+    }
+
+    public void otvori(View v){
+
+
+        db.collection("studenti").get().addOnCompleteListener(new OnCompleteListener<QuerySnapshot>() {
+            @Override
+            public void onComplete(@NonNull Task<QuerySnapshot> task) {
+                if (task.isSuccessful()) {
+
+                    for (QueryDocumentSnapshot document : task.getResult()) {
+                        Log.e("Rez", document.getId());
+                    }
+                }
+            }
+            }
+                );
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+    }
+}
+
+
+
+//        startActivity(new Intent(MainActivity.this, str2.class));
+//        Student std = new Student();
+//        String s= ((EditText)findViewById(R.id.editTextTextPersonName2)).getText().toString();
+//        String [] as = s.split("/");
+//        switch (as[0]){
+//            case "A1":
+//
+//                break;
+//            case "B1":
+//
+//                break;
+//            case "V1":
+//
+//                break;
+//        }
+
---
+++ app/src/main/java/kod/tsp/Student.java
@@ -0,0 +1,44 @@
+package kod.tsp;
+
+public class Student {
+    String ime;
+    String prezime;
+    int god;
+    String smer;
+
+    public Student() {
+
+    }
+
+    public String getIme() {
+        return ime;
+    }
+
+    public void setIme(String ime) {
+        this.ime = ime;
+    }
+
+    public String getPrezime() {
+        return prezime;
+    }
+
+    public void setPrezime(String prezime) {
+        this.prezime = prezime;
+    }
+
+    public int getGod() {
+        return god;
+    }
+
+    public void setGod(int god) {
+        this.god = god;
+    }
+
+    public String getSmer() {
+        return smer;
+    }
+
+    public void setSmer(String smer) {
+        this.smer = smer;
+    }
+}
